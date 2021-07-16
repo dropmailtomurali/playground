83e5c6acc11764ef221807ed5ca58be690862597
dropmailtomurali <45678121+dropmailtomurali@users.noreply.github.com>
2021-07-16 13:41:10 +1000

    Incremental Updates

---
+++ avatar-sample/app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def sample_avatar():
+	target = os.environ.get('TARGET', 'We are Team Avatar!')
+	return 'Hello {}!\n'.format(target)
+	
+if __name__ == "__main__":
+	app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
